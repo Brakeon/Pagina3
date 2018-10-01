ca930932dca2f88ae3f66448a9435a9c36258627
Asier Abad <Asier.abad@gmail.com>
2018-10-01 09:17:19 +0200

    inicio

---
+++ app.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var app = express();
+var http = require('http'),
+    fs = require('fs');
+
+app.get('/', function(req, res) {
+    res.send('Hola Mundos!');
+    res.redirect("/asdf")
+
+});
+
+app.get('/asdf', function(req, res) {
+    res.send('Hola Mundo!');
+});
+
+app.get('/users', function(req, res) {
+    var user;
+    // res.json({ user: 'tobi' });
+    res.json({ users: "usuario" }, { usuario: "user" });
+    //response.send('user ' + request.params.id);
+
+});
+
+/*app.get('/user/:id', function(req, res) {
+    var usuarios = { 1, "Asier" }
+    res.send('user ' + req.params.id);
+});*/
+
+var pid = [{ "id": 'Pepe', "edad": 23 }, { "id": 'Ana', "edad": 32 }, { "id": 'Jose', "edad": 34 }];
+
+app.get('/a/:id', function(req, res) {
+    var entrada = req.params.id;
+    var salida;
+
+    for (var num in pid) {
+
+        res.send(pid[num] + "\n");
+        if (pid[num].id == entrada) {
+
+            salida = pid[num].edad;
+        }
+    }
+
+    if (salida == null) {
+        res.send("no hay na de ná");
+    } else {
+        res.send('edad: ' + salida);
+    }
+});
+
+/*app.post('/profile', upload.array(), function(req, res, next) {
+    console.log(req.body);
+    res.json(req.body);
+});*/
+
+app.listen(3000, function() {
+    console.log('Aplicación ejemplo, escuchando el puerto 3000!');
+});
